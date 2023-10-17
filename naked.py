87b96f75b9808f9f8c4039a28676c755921627a1
Arnolds Helmanis <a.helmanis@gmail.com>
2023-10-17 21:02:45 +0000

    Izlaboju savus 'credentials' konfigurācijas failā.

--- naked.py
+++ naked.py
@@ -11,7 +11,7 @@ print('Asteroid processing service')
 print('Loading configuration from file')
 
 # 
-nasa_api_key = "???"
+nasa_api_key = "yF1V3ume64bRSuyDCnihnb6aMI4eeb8AHagROCJv"
 nasa_api_url = "https://api.nasa.gov/neo/"
 
 # Getting todays date
